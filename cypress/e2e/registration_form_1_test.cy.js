e93e43d09723a554fed911cfd794f05ed8f598e2
Riina-Retpap <riina.retpap2.0@gmail.com>
2024-01-31 16:21:10 +0200

    My comments about the commit

--- cypress/e2e/registration_form_1_test.cy.js
+++ cypress/e2e/registration_form_1_test.cy.js
@@ -6,7 +6,7 @@ beforeEach(() => {
 /*
 Assignment 2:
 
- 1. Update the name of test suite by adding you name: “This is first test suite, John Smith”
+ 1. Update the name of test suite by adding your name: “This is first test suite, John Smith”
  2. Replace text ‘Password123’ in the first test with your own chosen password (2 places) - passwords should match
  3. Change phone number in the first test to 555666777
  4. Change the order of steps in the first test:
@@ -16,13 +16,13 @@ Assignment 2:
  5. Add comment to the first test containing today’s date
  */
 
-describe('This is first test suite', () => {
+describe('This is first test suite, Riina Retpap', () => {
     it('User can submit data only when valid mandatory values are added', () => {
-        cy.get('#username').type('Something')
-        cy.get('[data-testid="phoneNumberTestId"]').type('5656565656')
-        cy.get('input[name="password"]').type('Password123')
-        cy.get('[name="confirm"]').type('Password123')
 
+        cy.get('[data-testid="phoneNumberTestId"]').type('555666777')
+        cy.get('input[name="password"]').type('EarthAndM00n')
+        cy.get('[name="confirm"]').type('EarthAndM00n')
+        cy.get('#username').type('Something')
         //in order to activate submit button, user has to click somewhere outside the input field
         cy.get('h2').contains('Password').click()
         cy.get('.submit_button').should('be.enabled')
@@ -38,14 +38,14 @@ describe('This is first test suite', () => {
         cy.get('#success_message').should('be.visible')
         cy.get('#success_message').should('have.css', 'display', 'block')
     });
-
+    // Today 31.01.2024
 
     it('User can use only same both first and validation passwords', () => {
-        cy.get('#username').type('johnDoe')
-        cy.get('[data-testid="phoneNumberTestId"]').type('10203040')
+
+        cy.get('[data-testid="phoneNumberTestId"]').type('56565656')
         cy.get('input[name="password"]').type('Password123')
         cy.get('[name="confirm"]').type('Password123123')
-        
+        cy.get('#username').type('johnDoe')
         // type('{enter}') is clicking native enter button from thekeyboard
         // for example, to click backspace use '{backspace}'
         cy.get('[name="confirm"]').type('{enter}')
@@ -100,19 +100,37 @@ describe('This is first test suite', () => {
         // Add test, similar to previous one with phone number field not filled in
         // All other fields should be entered correctly
         // Assert that submit button is not enabled and that successful message is not visible
+        
+        cy.get('#username').type('Chandler')
+        cy.get('[name="password"]').type('EarthAndM00n')
+        cy.get('[name="confirm"]').type('EarthAndM00n')
+        cy.get('.submit_button').should('not.be.enabled')
+        cy.get('#success_message').should('not.be.visible')
+
     })
 
     it('User cannot submit data when password and/or confirmation password is absent', () => {
         // Add test, similar to previous one with password field not filled in
         // All other fields should be entered correctly
         // Assert that submit button is not enabled and that successful message is not visible
+        cy.get('#username').type('Chandler')
+        cy.get('#phoneNumber').type('567895678')
+        cy.get('[name="confirm"]').type('EarthAndM00n')
+        cy.get('.submit_button').should('not.be.enabled')
+        cy.get('#success_message').should('not.be.visible')
+
     })
 
     it('User cannot add letters to phone number', () => {
         // Next verification is given as example
         // how we can check from html code, that phone number should contain only numbers
         cy.get('[data-testid="phoneNumberTestId"]').should('have.attr', 'type', 'number')
-
+        cy.get('#username').type('Chandler')
+        cy.get('#phoneNumber').type('Abba')
+        cy.get('[name="password"]').type('EarthAndM00n')
+        cy.get('[name="confirm"]').type('EarthAndM00n')
+        cy.get('.submit_button').should('not.be.enabled')
+        cy.get('#success_message').should('not.be.visible')
         // Add steps, when all fields are correctly filled in, except phone number
         // Try typing letters to phone number field
         // Assert that submit button is not enabled and that successful message is not visible
